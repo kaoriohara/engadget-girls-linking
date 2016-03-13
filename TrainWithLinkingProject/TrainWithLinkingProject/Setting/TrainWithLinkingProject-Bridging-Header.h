8daa7fcc5e59e3dc4cf2c4c5ee95e44490650ccc
momochaaam <kqss.251@gmail.com>
2016-03-13 19:53:54 +0900

    プロジェクト作成

---
+++ TrainWithLinkingProject/TrainWithLinkingProject/Setting/TrainWithLinkingProject-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  TrainWithLinkingProject-Bridging-Header.h
+//  TrainWithLinkingProject
+//
+//  Created by Aimi Kasama on 2016/03/13.
+//  Copyright © 2016年 LinkingTeam1. All rights reserved.
+//
+
+#ifndef TrainWithLinkingProject_Bridging_Header_h
+#define TrainWithLinkingProject_Bridging_Header_h
+
+
+#endif /* TrainWithLinkingProject_Bridging_Header_h */
+
+#import <LinkingLibrary/LinkingLibrary.h>
